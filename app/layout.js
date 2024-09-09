20294659dc7dbca0b0c1e60666c1c6a37d07f298
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-09 16:25:12 +0600

    Font Optimization

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,20 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap"
+});
 
 export const metadata = {
-    title: "Rendering",
-    description: "Understanding Next.js Rendering process",
+  title: "Rendering",
+  description: "Understanding Next.js Rendering process",
 };
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
+    </html>
+  );
 }

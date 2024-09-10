c90f3c0cfd9256b207fddbb598836eb972a7a3fd
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-10 18:39:08 +0600

    Third-party Libraries

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import {GoogleTagManager, GoogleAnalytics, GoogleMapsEmbed, YouTubeEmbed} from "@next/third-parties/google";
 
 export const metadata = {
     title: "My website",
@@ -17,11 +18,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <GoogleTagManager gtmId="GTM-XYZ" />
             <body className="bg-gray-200 px-5">
                 <main className="mx-auto rounded bg-white max-w-5xl px-5 min-h-[100dvh]">
                     {children}
                 </main>
+                <YouTubeEmbed videoid="MCdVbYFAupg" height={400} params="controls=0" />
             </body>
+            <GoogleAnalytics gaId="G-XYZ" />
         </html>
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
+import { EventButton } from "./components/Button";
+
 export default async function Home() {
-    return (
-        <div className="p-8">
-            <h1 className="text-gray-800">Metadata</h1>
-            <div className="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
-                amet consequatur odio, obcaecati laudantium possimus dolorem
-                deserunt, molestiae quae voluptatem officia. Sit similique
-                sapiente sunt beatae modi perferendis vero nam.
-            </div>
-        </div>
-    );
+  return (
+    <div className="p-8">
+      <h1 className="text-gray-800">Metadata</h1>
+      <div className="mt-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, amet
+        consequatur odio, obcaecati laudantium possimus dolorem deserunt,
+        molestiae quae voluptatem officia. Sit similique sapiente sunt beatae
+        modi perferendis vero nam.
+      </div>
+
+      <div className="mt-6">
+        <EventButton />
+      </div>
+    </div>
+  );
 }
